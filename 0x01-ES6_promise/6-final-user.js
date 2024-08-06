9b4f30662e2d826f197823a71d5b0c392e5edb65
MarioRaafat <mariorafat10@gmail.com>
2024-08-06 11:58:56 -0700

    task6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+import uploadPhoto from "./5-photo-reject.js";
+import signUpUser from "./4-user-promise.js";
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName))
+    .then((res) => (
+      res.map((ele) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
+}
