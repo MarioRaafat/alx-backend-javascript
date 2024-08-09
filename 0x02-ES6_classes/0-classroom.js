d38a799ec105a29ad73cad491558d574a5f1c339
MarioRaafat <mariorafat10@gmail.com>
2024-08-08 19:14:14 -0700

    task0

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,12 @@
+/**
+ * Represents a class.
+ */
+export default class ClassRoom {
+  /**
+   * Creates a class named classroom.
+   * @param {Number} maxStudentsSize - Num in the class.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
