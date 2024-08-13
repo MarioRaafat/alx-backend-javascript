1996460155c50738f4340d14e151b64f08aebe01
MarioRaafat <mariorafat10@gmail.com>
2024-08-13 08:49:59 -0700

    task1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -8,9 +8,9 @@
  * @author Bezaleel Olakunori <https://github.com/B3zaleel>
  * @returns
  */
-export default function getListStudentIds (arr) {
-  if (arr instanceof Array) {
-    return arr.map((student) => student.id);
+export default function getListStudentIds(students) {
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
   }
   return [];
 }
