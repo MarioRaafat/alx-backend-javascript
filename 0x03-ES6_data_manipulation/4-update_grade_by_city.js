946919521cf379058466ed8313a4181d0a25efcb
MarioRaafat <mariorafat10@gmail.com>
2024-08-13 09:46:59 -0700

    task4

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,30 @@
+/**
+ * Updates the grades of a list of students in a given city.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @param {*} city - The city of students.
+ * @param {{
+ *   studentId: Number,
+ *   grade: Number,
+ * }[]} newGrades - The new grades to be given to a student.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
+export default function updateStudentGradeByCity (arr, city, obj) {
+  arr.filter((student) => student.location === city)
+    .map((student) => {
+      const g = obj.filter((grade) => grade.studentId === student.id);
+      if (g.length > 0) {
+        student.grade = g[0].grade;
+      } else {
+        student.grade = 'N/A';
+      }
+
+      return student;
+    });
+
+  return arr;
+}
