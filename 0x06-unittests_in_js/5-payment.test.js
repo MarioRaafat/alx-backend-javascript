adc6f8f43e13e119e97e3e6e83fd1d82b2c0770d
Mario Raafat <mariorafat10@gmail.com>
2024-10-04 14:30:34 +0300

    task5

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,29 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+    let consoleSpy;
+
+    beforeEach(() => {
+        if (!consoleSpy) {
+        consoleSpy = sinon.spy(console, 'log');
+        }
+    });
+
+    afterEach(() => {
+        consoleSpy.resetHistory();
+    });
+
+    it('sendPaymentRequestToApi(100, 20) logs "The total is: 120" to the console', () => {
+        sendPaymentRequestToApi(100, 20);
+        expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
+        expect(consoleSpy.calledOnce).to.be.true;
+    });
+
+    it('sendPaymentRequestToApi(10, 10) logs "The total is: 20" to the console', () => {
+        sendPaymentRequestToApi(10, 10);
+        expect(consoleSpy.calledWith('The total is: 20')).to.be.true;
+        expect(consoleSpy.calledOnce).to.be.true;
+    });
+});
